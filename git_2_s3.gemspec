add97de724a25369b53076f340e84dc575709b7b
Kurt Didenhover <kurt.didenhover@mediaforge.com>
2010-08-12 13:37:02 -0600

    second commit

---
+++ git_2_s3.gemspec
@@ -0,0 +1,19 @@
+Gem::Specification.new do |s|
+
+  s.name = 'git_2_s3'
+  s.version = '0.1.1'
+  s.platform = Gem::Platform::RUBY
+  s.summary = "A deployment strategy to S3 from github using capistrano"
+  s.description = "A deployment strategy that takes a github repository and pushes the files into an S3 bucket using capistrano"
+
+  s.files = Dir.glob("{bin,lib,examples,test}/**/*") + %w(README MIT-LICENSE)
+  s.has_rdoc = true
+
+  s.add_dependency 'capistrano', ">= 2.1.0"
+  s.add_dependency 's3sync'
+
+  s.author = "Kurt Didenhover"
+  s.email = "kdidenhover@gmail.com"
+  s.rubyforge_project = nil
+  s.homepage = "http://github.com/heyaustria/git-2-s3"
+end
---
+++ test/deploy_s3_bucket_test.rb
@@ -0,0 +1,54 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+require 'capistrano/logger'
+require 'capistrano/recipes/deploy/strategy/s3_bucket'
+require 'stringio'
+
+context "Capistrano::Deploy::Strategy::S3Bucket" do
+  def setup
+    logger = Capistrano::Logger.new(:output => StringIO.new)
+    # Uncomment this line to see the output a cap user would see for debugging
+     logger = Capistrano::Logger.new
+    logger.level = Capistrano::Logger::MAX_LEVEL
+    @config = { :application => "testapp",
+                :logger => logger,
+                :releases_path => "/u/apps/test/releases",
+                :release_path => "/u/apps/test/releases/1234567890",
+                :real_revision => "154",
+                :deploy_s3_bucket => 'com.example.bucket',
+                :s3_config => {'AWS_ACCOUNT_NUMBER' => '1234-5678-9012', 
+                               'AWS_ACCESS_KEY_ID' => 'ABCDEFGHIJKLMNOPQRST', 
+                               'AWS_SECRET_ACCESS_KEY' => 'abcdefghijklmnopqrstuvwxyz01234567890ABC' }
+                 }
+    @source = mock("source")
+    @config.stubs(:source).returns(@source)
+    @config.stubs(:set)
+    @strategy = Capistrano::Deploy::Strategy::S3Bucket.new(@config)
+    Dir.stubs(:tmpdir).returns("/tmp/dir")
+  end
+
+  specify "does push to S3" do
+    @strategy.expects(:`).returns("--------------------\n")    
+    prepare_deploy
+    @source.expects(:checkout).with("154", "/tmp/dir/1234567890").returns(:local_checkout)
+    @strategy.expects(:system).with(:local_checkout)
+    Dir.expects(:chdir).with("/tmp/dir/1234567890").yields
+    @strategy.expects(:system).with('s3cmd put com.example.bucket:testapp_154.tgz /tmp/dir/testapp_154.tgz')
+    @strategy.deploy!
+  end
+
+private
+
+  def aws_credential_envs
+    "AWS_ACCOUNT_NUMBER=1234-5678-9012 AWS_ACCESS_KEY_ID=ABCDEFGHIJKLMNOPQRST AWS_SECRET_ACCESS_KEY=abcdefghijklmnopqrstuvwxyz01234567890ABC"
+  end
+
+  # These are expected for any deploy
+  def prepare_deploy
+    @strategy.expects(:run).with("#{aws_credential_envs} s3cmd get com.example.bucket:testapp_154.tgz /tmp/1234567890.tar.gz")
+    @strategy.expects(:run).with("mkdir /u/apps/test/releases/1234567890 && cd /u/apps/test/releases/1234567890 && tar xzf /tmp/1234567890.tar.gz && rm /tmp/1234567890.tar.gz")
+  end
+
+end 
+ 
+  
